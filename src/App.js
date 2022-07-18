7a6f408c5d587a055d13f65ac7749f5fa6d49af2
sadikhossain69 <sadikhossain433@gmail.com>
2022-07-18 20:14:15 +0600

    navbar done by sadik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 	return (
 		<div>
+			<Navbar/>
 			<h1>Hello western lab university</h1>
 			<p>hello</p>
 			<p>Noo way</p>
