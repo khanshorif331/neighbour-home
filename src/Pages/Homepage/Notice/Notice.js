2618832ade1537d24b0f95fb1b6213a35b9ab758
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-07-20 23:20:28 +0600

    notice section responsive issue solved

--- src/Pages/Homepage/Notice/Notice.js
+++ src/Pages/Homepage/Notice/Notice.js
@@ -4,17 +4,15 @@ import Marquee from "react-fast-marquee";
 const Notice = () => {
   return (
     <div className="flex items-center justify-start my-7 bg-gray-100 mt-28">
-      <h2 className="uppercase text-2xl bg-[#0D9488] px-2 py-5 text-white font-bold w-32">
+      <h2 className="uppercase lg:text-2xl text-xl bg-[#0D9488] px-2 py-5 text-white font-bold w-32 ">
         Notice :
       </h2>
       <Marquee gradient={false} speed={50} gradientWidth={0}>
-        <p className="text-2xl ">
           {" "}
-          <blockquote>
+          <blockquote className="text-2xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
             odit placeat facilis dolores odio error repellendus facere. 
           </blockquote>
-        </p>
       </Marquee>
     </div>
   );
