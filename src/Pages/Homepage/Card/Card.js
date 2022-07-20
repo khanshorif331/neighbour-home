b84127219e51207f69e05376d4a5f152818122f0
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-07-20 19:08:31 +0600

    responsive issue solved in engineer section

--- src/Pages/Homepage/Card/Card.js
+++ src/Pages/Homepage/Card/Card.js
@@ -4,13 +4,13 @@ const Card = ({ course }) => {
   const { title, picture } = course;
   return (
     <div>
-      <div class="max-w-sm bg-white px-6 pt-6 pb-2 border shadow-lg transform hover:scale-105 transition duration-500">
-        <div class="relative">
-          <img class="w-full h-52" src={picture} alt="Colors" />
-          {/* <p class="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
+      <div className="max-w-sm bg-white px-6 pt-6 pb-2 border shadow-lg transform hover:scale-105 transition duration-500">
+        <div className="relative">
+          <img className="w-full h-52" src={picture} alt="Colors" />
+          {/* <p className="absolute top-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-br-lg rounded-tl-lg">
             {price}
           </p> */}
-          {/* <p class="absolute top-0 right-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-tr-lg rounded-bl-lg">
+          {/* <p className="absolute top-0 right-0 bg-yellow-300 text-gray-800 font-semibold py-1 px-3 rounded-tr-lg rounded-bl-lg">
             {discount}
           </p> */}
         </div>
@@ -21,8 +21,8 @@ const Card = ({ course }) => {
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem,
           itaque!
         </p>
-        <div class="my-4">
-          <button class="mt-4 text-xl w-full text-white bg-[#0D9488] py-2 rounded shadow-lg">
+        <div className="my-4">
+          <button className="mt-4 text-xl w-full text-white bg-[#0D9488] py-2 rounded shadow-lg">
             Details
           </button>
         </div>
