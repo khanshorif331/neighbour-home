facbecd43ba9de9ad2104d67ac9cfd5963b5c6ad
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-07-25 22:12:08 +0600

    reviw data make daynamic by rakib

--- src/Pages/Homepage/AboutUs/AboutUs.js
+++ src/Pages/Homepage/AboutUs/AboutUs.js
@@ -3,20 +3,20 @@ import {GiCheckMark} from 'react-icons/gi';
 
 const AboutUs = () => {
      return (
-          <div className='py-16 px-20'>
+          <div  style={{ fontFamily: "'Rajdhani', sans-serif" }} className='py-16 px-20'>
                <h2 className="uppercase text-3xl font-bold text-center">
                     About Us
                </h2>
-               <div className="flex justify-center mb-10">
+               <div className="flex justify-center mb-16">
                     <div className=" rounded-md w-32 h-1 bg-[#0D9488]"></div>
                     <div className=" rounded-md w-4 h-1 mx-2 bg-[#0D9488]"></div>
                     <div className=" rounded-md w-2 h-1 bg-[#0D9488]"></div>
                </div>
-          <div className='flex  '>
+          <div className='flex capitalize'>
                <div className='w-5/12'>
                <div className='relative w-9/12 h-[420px] mx-auto  '>
-                    <img src="https://hgrealestate.net/wp-content/uploads/2022/02/Team-700x1000-1.jpg" alt="" className="w-full overflow-hidden h-full" />
-                    <div className='flex items-center justify-center flex-col font-semibold text-white  bg-teal-900 h-[130px] w-[130px] rounded-full absolute top-[-30px] right-[-30px]'>
+                    <img src="https://verint.imgix.net/wp-content/uploads/definitive-bo-13-800x1000-1.jpg?fit=max&auto=format&auto=compress" alt="" className="w-full overflow-hidden h-full" />
+                    <div className='flex items-center justify-center flex-col font-semibold text-white  bg-teal-900 h-[130px] w-[130px] rounded-full absolute top-[-40px] right-[-40px]'>
                          <h2 className='text-3xl font-bold'>25</h2>
                          <p>Years</p>
                          <p>Exprience</p>
@@ -34,7 +34,7 @@ const AboutUs = () => {
                          <li className='flex items-center my-1 w-5/12'> <GiCheckMark className='text-green-700 mr-2 '></GiCheckMark> 24+7 Emergency Service</li>
                     </ul>
                     <button
-                         className='px-8 py-3 bg-teal-900 hover:bg-white hover:text-teal-900  transition text-white rounded inline-block mt-4 sm:mt-8 font-semibold'>
+                         className='px-8 py-3 border-2 border-teal-900 bg-teal-900 hover:bg-white hover:text-teal-900  duration-200 text-white rounded inline-block mt-4 sm:mt-8 font-semibold'>
                          Explore Now
                     </button>
                </div>
