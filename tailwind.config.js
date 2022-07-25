ac3c2f7c2d1e900ab54768a5514c8b46cb72493f
Nayeem Hasan <hasannayeeem@gmail.com>
2022-07-25 23:02:51 +0600

    daisyui added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+	  daisyui: {
+		themes: [
+		  {
+			neighborTheme: {
+				primary: "#a991f7",
+				secondary: "#f6d860",
+				accent: "#37cdbe",
+				neutral: "#3d4451",
+				"base-100": "#ffffff",
+			},
+		  },
+		  
+		  "dark",
+		  "cupcake",
+		],
+	  },
+	plugins: [require("daisyui")],
 }
