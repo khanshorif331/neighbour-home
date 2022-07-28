185e6cf77deac88cb0be1392321a2ea2f4ff039e
khanshorif331 <khanshorif331@gmail.com>
2022-07-29 03:51:24 +0600

    constructor table added

---
+++ src/Pages/Dashboardpage/Constructors/Constructor.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Constructor = () => {
+	return (
+		<tr>
+			<th>1</th>
+			<td>Cy Ganderton</td>
+			<td>Quality Control Specialist</td>
+			<td>Littel, Schaden and Vandervort</td>
+			<td>Canada</td>
+			<td>12/16/2020</td>
+			<td>Blue</td>
+		</tr>
+	)
+}
+
+export default Constructor
--- src/Pages/Dashboardpage/Constructors/Constructors.js
+++ src/Pages/Dashboardpage/Constructors/Constructors.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
+import Constructor from './Constructor'
 
 const Constructors = () => {
-	const [constructor, setConstructor] = useState([])
+	const [constructors, setConstructors] = useState([])
 	// const { isLoading, error, data } = useQuery('constructor', () =>
 	// 	fetch('https://neighbour-home--server.herokuapp.com/constructor').then(
 	// 		res => res.json()
@@ -17,12 +18,48 @@ const Constructors = () => {
 	useEffect(() => {
 		fetch('https://neighbour-home--server.herokuapp.com/constructor')
 			.then(res => res.json())
-			.then(data => setConstructor(data))
+			.then(data => setConstructors(data))
 	}, [])
 
 	return (
-		<div>
+		<div className='sm:px-10 px-2 pb-5'>
 			<h1>Heelo world</h1>
+			{/* starting the table */}
+
+			<div class='overflow-x-auto'>
+				<table class='table table-compact w-full'>
+					<thead>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Job</th>
+							<th>company</th>
+							<th>location</th>
+							<th>Last Login</th>
+							<th>Favorite Color</th>
+						</tr>
+					</thead>
+					<tbody>
+						{constructors.map(constructor => (
+							<Constructor
+								key={constructor._id}
+								constructor={constructor}
+							></Constructor>
+						))}
+					</tbody>
+					<tfoot>
+						<tr>
+							<th></th>
+							<th>Name</th>
+							<th>Job</th>
+							<th>company</th>
+							<th>location</th>
+							<th>Last Login</th>
+							<th>Favorite Color</th>
+						</tr>
+					</tfoot>
+				</table>
+			</div>
 		</div>
 	)
 }
