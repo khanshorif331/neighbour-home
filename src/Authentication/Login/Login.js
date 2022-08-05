ecc4ec3b7d1e7fa91852794ec0887bb210a69395
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-05 20:47:17 +0600

    border color upgrade in login and register form

--- src/Authentication/Login/Login.js
+++ src/Authentication/Login/Login.js
@@ -58,7 +58,7 @@ const Login = () => {
 
                 <div className="w-full flex flex-wrap flex-row-reverse">
 
-                    <div className="w-full md:w-1/2 flex flex-col border-l-2 border-l-gray-400">
+                    <div className="w-full md:w-1/2 flex flex-col border-l-2 border-l-gray-500">
 
                         <div className="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
                             <p className="text-center text-3xl">Login Here!</p>
