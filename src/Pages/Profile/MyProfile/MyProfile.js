72bea15534deeddd3c80bc1d4d93f0cd04d4a43a
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-08 23:44:05 +0600

    my profile page created and decrease loading time

---
+++ src/Pages/Profile/MyProfile/MyProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProfile = () => {
+    return (
+        <section>
+            <h1>My Profile</h1>
+        </section>
+    );
+};
+
+export default MyProfile;
