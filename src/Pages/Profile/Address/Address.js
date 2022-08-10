471ca17e76ec39cb383a4a6c767995869a71856b
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-10 09:15:29 +0600

    my address section design

--- src/Pages/Profile/Address/Address.js
+++ src/Pages/Profile/Address/Address.js
@@ -4,7 +4,7 @@ const Address = () => {
     return (
         <section>
             <h1 className='text-center text-3xl py-3 font-bold border-b-2 border-b-gray-300'>My Address</h1>
-            <div className='flex md:flex-row flex-col md:p-10 p-4 '>
+            {/* <div className='flex md:flex-row flex-col md:p-10 p-4 '>
                 <div className='md:pr-28'>
                     <h2 className='font-semibold text-blue-600'>Present Address</h2>
                     <div className='grid lg:grid-cols-2 grid-cols-1 md:mt-5'>
@@ -39,10 +39,41 @@ const Address = () => {
                         </div>
                     </div>
                 </div>
-            </div>
-                <div className='text-center'>
-                    <button className='bg-blue-600 font-semibold py-2 px-5 text-white rounded-full'>Edit Address</button>
+            </div> */}
+            <form>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 p-4'>
+                    <div>
+                        <label className='font-medium'>Your Current Country</label>
+                        <input type="text" placeholder="Bangladesh" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <label className='font-medium'>Your Current Town</label>
+                        <input type="text" placeholder="Dhaka" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <label className='font-medium'>Your Current Zip Code</label>
+                        <input type="text" placeholder="64611" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <label className='font-medium'>Your Permanent Country</label>
+                        <input type="text" placeholder="USA" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <label className='font-medium'>Your Permanent Town</label>
+                        <input type="text" placeholder="New York" class="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div>
+                        <label className='font-medium'>Your Permanent Zip</label>
+                        <input type="text" placeholder="75482" class="input input-bordered w-full max-w-xs" />
+                    </div>
                 </div>
+                <div className='text-center mb-1'>
+                    <button className='bg-green-600 font-semibold py-2 px-5 text-white rounded-full'>Update</button>
+                </div>
+            </form>
+            <div className='text-center'>
+                <button className='bg-blue-600 font-semibold py-2 px-5 text-white rounded-full'>Edit Address</button>
+            </div>
         </section>
     );
 };
