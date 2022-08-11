cffebb36c4cce5182cb21a67867b4c7704586102
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-08-11 19:51:23 +0600

    content uploaded

--- src/Pages/Homepage/Pricing/Pricing.js
+++ src/Pages/Homepage/Pricing/Pricing.js
@@ -28,7 +28,8 @@ const Pricing = () => {
                   <span className={`text-6xl font-bold`}>Free</span>
                 </div>
                 <p className="mt-3 leading-relaxed text-gray-600">
-                  Etiam ac convallis enim, eget euismod dolor.
+                  This package is for small business. If you want to growth your
+                  company quickly please upgrade your package.
                 </p>
                 <ul className="flex-1 mb-6 text-gray-600">
                   <li className="flex mb-2 space-x-2">
@@ -44,7 +45,7 @@ const Pricing = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Aenean quis</span>
+                    <span>Buy properties</span>
                   </li>
                   <li className="flex mb-2 space-x-2">
                     <svg
@@ -59,7 +60,7 @@ const Pricing = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Morbi semper</span>
+                    <span>contact with engineers</span>
                   </li>
                   <li className="flex mb-2 space-x-2">
                     <svg
@@ -74,7 +75,22 @@ const Pricing = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    <span>Tristique enim nec</span>
+                    <span>Manage your works</span>
+                  </li>
+                  <li className="flex mb-2 space-x-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="flex-shrink-0 w-6 h-6 text-sky-600"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>And more updated features</span>
                   </li>
                 </ul>
                 <button
@@ -96,14 +112,15 @@ const Pricing = () => {
                 <div className={` ${darkMode && "text-gray-900"} space-y-2`}>
                   <h4 className="text-2xl font-bold">Pro</h4>
                   <span className="text-6xl font-bold">
-                    $24
+                    $199
                     <span className="text-sm tracking-wide">/month</span>
                   </span>
                 </div>
                 <p
                   className={` ${darkMode && "text-gray-900"} leading-relaxed`}
                 >
-                  Morbi cursus ut sapien sit amet consectetur.
+                  This package is use for mid-level business company. For large
+                  scale company you can go our <b>"Business"</b> package.
                 </p>
                 <ul
                   className={` ${darkMode && "text-gray-900"} flex-1 space-y-2`}
@@ -188,14 +205,15 @@ const Pricing = () => {
                 } flex flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-gray-50`}
               >
                 <div className="space-y-2">
-                  <h4 className={`text-2xl font-bold`}>Team</h4>
+                  <h4 className={`text-2xl font-bold`}>Business</h4>
                   <span className={`text-6xl font-bold`}>
-                    $72
+                    $372
                     <span className="text-sm tracking-wide">/month</span>
                   </span>
                 </div>
                 <p className="leading-relaxed text-gray-600">
-                  Phasellus ultrices bibendum nibh in vehicula.
+                  This package is for Large scale company who can only get our
+                  fully premium package.
                 </p>
                 <ul className="space-y-2 text-gray-600">
                   <li className="flex items-start space-x-2">
