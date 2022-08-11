32dbdd24e2c353fd78bf14d9e07570e4cf31adb7
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-08-11 18:34:13 +0600

    pricing section design done

--- src/Pages/Homepage/Pricing/Pricing.js
+++ src/Pages/Homepage/Pricing/Pricing.js
@@ -5,19 +5,19 @@ const Pricing = () => {
   const [darkMode] = useContext(DarkModeContext);
   return (
     <div>
-      <section className="py-20  text-gray-800">
+      <section className="py-20 ">
         <div className="container px-4 mx-auto">
           <div className="max-w-2xl mx-auto mb-16 text-center">
             <h2
               className={`${
-                darkMode && "text-gray-50"
-              }text-4xl font-bold lg:text-5xl text-gray-900`}
+                darkMode && "text-red-600"
+              } text-4xl font-bold lg:text-5xl text-black`}
             >
               Choose your best plan
             </h2>
           </div>
           <div className="flex flex-wrap items-stretch -mx-4">
-            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0 transform hover:scale-105 transition duration-500">
               <div
                 className={`${
                   darkMode && "dark:bg-gray-900 text-gray-50"
@@ -87,28 +87,28 @@ const Pricing = () => {
                 </button>
               </div>
             </div>
-            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+            <div className="flex w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0 transform hover:scale-105 transition duration-500">
               <div
-                className={`${
-                  darkMode && "dark:bg-sky-400"
-                }flex flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-sky-600 text-gray-50`}
+                className={` ${
+                  darkMode && "bg-sky-400 text-gray-900"
+                } flex flex-col p-6 space-y-6 rounded shadow sm:p-8 bg-sky-600 text-gray-50`}
               >
-                <div className={`${darkMode && "text-gray-900"} space-y-2`}>
+                <div className={` ${darkMode && "text-gray-900"} space-y-2`}>
                   <h4 className="text-2xl font-bold">Pro</h4>
                   <span className="text-6xl font-bold">
                     $24
                     <span className="text-sm tracking-wide">/month</span>
                   </span>
                 </div>
-                <p className={`${darkMode && "text-gray-900"} leading-relaxed`}>
+                <p
+                  className={` ${darkMode && "text-gray-900"} leading-relaxed`}
+                >
                   Morbi cursus ut sapien sit amet consectetur.
                 </p>
-                <ul className="flex-1 space-y-2">
-                  <li
-                    className={`${
-                      darkMode && "text-gray-900"
-                    } text-gray-100 flex items-center space-x-2`}
-                  >
+                <ul
+                  className={` ${darkMode && "text-gray-900"} flex-1 space-y-2`}
+                >
+                  <li className="flex items-center space-x-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
@@ -123,11 +123,7 @@ const Pricing = () => {
                     </svg>
                     <span>Everything in Free</span>
                   </li>
-                  <li
-                    className={`${
-                      darkMode && "text-gray-900"
-                    } text-gray-100 flex items-center space-x-2`}
-                  >
+                  <li className="flex items-center space-x-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
@@ -142,11 +138,7 @@ const Pricing = () => {
                     </svg>
                     <span>Phasellus tellus</span>
                   </li>
-                  <li
-                    className={`${
-                      darkMode && "text-gray-900"
-                    } text-gray-100 flex items-center space-x-2`}
-                  >
+                  <li className="flex items-center space-x-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
@@ -161,11 +153,7 @@ const Pricing = () => {
                     </svg>
                     <span>Praesent faucibus</span>
                   </li>
-                  <li
-                    className={`${
-                      darkMode && "text-gray-900"
-                    } text-gray-100 flex items-center space-x-2`}
-                  >
+                  <li className="flex items-center space-x-2">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
@@ -182,17 +170,18 @@ const Pricing = () => {
                   </li>
                 </ul>
                 <a
-                  rel="noopener referrer"
+                  rel="noopener noreferrer"
                   href="#"
-                  className={`${
-                    darkMode && "dark:bg-gray-800 dark:text-sky-400"
-                  } inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-gray-100 text-sky-600 bottom-0`}
+                  className={` ${
+                    darkMode && "bg-gray-900"
+                  } inline-block w-full px-5 py-3 font-bold tracking-wider text-center rounded bg-gray-100 text-sky-600`}
                 >
                   Get Started
                 </a>
               </div>
             </div>
-            <div className="w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0">
+
+            <div className="w-full mb-8 sm:px-4 md:w-1/2 lg:w-1/3 lg:mb-0 transform hover:scale-105 transition duration-500">
               <div
                 className={`${
                   darkMode && "dark:bg-gray-900 text-gray-50"
