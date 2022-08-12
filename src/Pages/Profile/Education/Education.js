c2deb1722ec1e1d51687ae99960c2d11acf5eab3
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-12 09:31:58 +0600

    profile education route created

---
+++ src/Pages/Profile/Education/Education.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Education = () => {
+    return (
+        <section>
+            <p>This is Education!!!</p>
+        </section>
+    );
+};
+
+export default Education;
