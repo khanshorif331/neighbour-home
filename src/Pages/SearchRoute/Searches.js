1d8a8572de2fe185df2926ce6a5c05e508ea435d
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-17 13:34:35 +0600

    conflict issue fixed

--- src/Pages/SearchRoute/Searches.js
+++ src/Pages/SearchRoute/Searches.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Searches = () => {
     return (
         <section className='mt-[80px] sm:mt-[129px]'>
-            <p>This is Searches!!!</p>
+            <p>This is Searches!!!</p> 
         </section>
     );
 };
