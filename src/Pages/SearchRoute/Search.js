551f4dcc4a56e2c54fe63e792637b7f9e840b69a
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-18 23:04:26 +0600

    design done for search result

--- src/Pages/SearchRoute/Search.js
+++ src/Pages/SearchRoute/Search.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-const Search = ({search}) => {
+const Search = ({ search }) => {
 
     console.log(search)
-    const {body, email, name} = search
+    const { body, email, name } = search
 
     return (
         <>
-            <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-                <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://placeimg.com/192/192/people" alt="" width="384" height="512"/>
+            <div className='px-3 md:px-5 py-2 rounded'>
+                <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+                    <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://placeimg.com/192/192/people" alt="" width="384" height="512" />
                     <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
                         <blockquote>
                             <p class="text-lg font-medium">
@@ -24,7 +25,8 @@ const Search = ({search}) => {
                             </div>
                         </figcaption>
                     </div>
-            </figure>
+                </figure>
+            </div>
         </>
     );
 };
