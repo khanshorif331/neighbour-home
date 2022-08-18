564829897eb0f394f1ebab8090746c9de1cc1eb1
sadikhossain69 <sadikhossain433@gmail.com>
2022-08-18 22:27:46 +0600

    search button inplement

--- src/Pages/SearchRoute/Searches.js
+++ src/Pages/SearchRoute/Searches.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './Searches.css'
 
 const Searches = () => {
     return (
         <section className='mt-[80px] sm:mt-[129px]'>
+            <p>This is Search Bar</p>
+            <div className="group">
+                <svg className="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+                <input placeholder="Search" type="search" className="input"/>
+            </div>
             {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
                 <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://placeimg.com/192/192/people" alt="" width="384" height="512"/>
                     <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
