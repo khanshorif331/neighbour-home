c5559775725f1973798b5446a0cd0176c8ffe098
khanshorif331 <khanshorif331@gmail.com>
2022-08-26 19:42:28 +0600

    fixed some bug

--- src/hooks/useRole.js
+++ src/hooks/useRole.js
@@ -1,23 +1,25 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-const useRole = (user) => {
-    const [role, setrole] = useState()
-    const [roleLoading, setRoleLoading] = useState(false)
-    // console.log(role);
-    useEffect(() => {
-        if (user) {
-            setRoleLoading(true)
-            axios.get(`http://neighbour-home--server.herokuapp.com/singleUserByEmail/${user?.email}`)
-                .then(data => {
-                    // console.log(data.data);
-                    setrole(data?.data?.role)
-                    setRoleLoading(false)
-                })
-        }
+const useRole = user => {
+	const [role, setrole] = useState()
+	const [roleLoading, setRoleLoading] = useState(false)
+	// console.log(role);
+	useEffect(() => {
+		if (user) {
+			setRoleLoading(true)
+			axios
+				.get(
+					`https://neighbour-home--server.herokuapp.com/singleUserByEmail/${user?.email}`
+				)
+				.then(data => {
+					// console.log(data.data);
+					setrole(data?.data?.role)
+					setRoleLoading(false)
+				})
+		}
+	}, [user])
 
-    }, [user]);
-
-    return [role, roleLoading]
+	return [role, roleLoading]
 }
-export default useRole;
+export default useRole
